a16e45b768867a025e577652194b878f6e97006d
Frank Chambergo <franky4908@gmail.com>
2020-09-16 22:49:46 -0400

    http params for parameters

--- youtube-citer/src/app/citation-form/citation-form.component.ts
+++ youtube-citer/src/app/citation-form/citation-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeDataAPI } from '../shared/youtube-data-api.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { YoutubeVideoData } from '../shared/youtube-video-data.model';
 
 @Component({
   selector: 'app-citation-form',
@@ -9,6 +10,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CitationFormComponent implements OnInit {
   submitForm: FormGroup;
+  videoData: YoutubeVideoData;
 
   constructor(public service: YoutubeDataAPI,
     public fb: FormBuilder) {
@@ -16,7 +18,9 @@ export class CitationFormComponent implements OnInit {
 
   getVideoInfo(id: string){
     this.service.getVideoInfo(id).subscribe(info => {
-      console.log(info);
+      this.videoData = info;
+      console.log(this.videoData);
+      
     })
   }
 
--- youtube-citer/src/app/shared/youtube-data-api.service.ts
+++ youtube-citer/src/app/shared/youtube-data-api.service.ts
@@ -8,14 +8,17 @@ export class YoutubeDataAPI {
   constructor(public http: HttpClient) { }
 
   API_KEY: string = "AIzaSyABqlR07Xv1sI89Mx-WdBB72-C8h2ucGiU";
-  baseUrl: string = "https://www.googleapis.com/youtube/v3/";
+  baseUrl: string = "https://www.googleapis.com/youtube/v3/videos";
 
   getVideoInfo(id: string) {
+    let params = new HttpParams();
+    params = params.set('part', 'snippet').set('id', id).set('key', this.API_KEY);
+
     let headers = new HttpHeaders({
       'Content-Type': "application/json"
     });
 
-    return this.http.get<any>(this.baseUrl + 'videos?part=snippet&id=' + id + '&key=' + this.API_KEY, 
+    return this.http.get<any>(this.baseUrl + "?" + params, 
       { headers: headers }
       ).pipe(
           map(
