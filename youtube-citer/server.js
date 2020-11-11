ee9ba09b2de8708a2cb2698e2ef0025e97e84ce3
Frank Chambergo <franky4908@gmail.com>
2020-11-10 23:47:50 -0500

    directory correction

--- youtube-citer/server.js
+++ youtube-citer/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/youtube-citer'));
+app.use(express.static(__dirname + '/dist/youtube-citer/youtube-citer'));
  
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/youtube-citer/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/youtube-citer/youtube-citer/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
