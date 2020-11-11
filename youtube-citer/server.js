64a7cfa5f9841b6942d93c6106a5d609e7fdc958
Frank Chambergo <franky4908@gmail.com>
2020-11-11 17:54:33 -0500

    Deployment config

--- youtube-citer/server.js
+++ youtube-citer/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/youtube-citer/dist'));
  
 app.get('*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/youtube-citer/index.html'));
+  res.sendFile(path.join(__dirname + 'youtube-citer/dist/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
